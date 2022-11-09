1c1f8a6bcf591f903243957f5f5d2f91060f5027
marinred <endrms1995@gmail.com>
2022-11-09 17:41:43 +0900

    프로그머스 1주차 1번_1주차 2번 완료

---
+++ week_1/program_1.py
@@ -0,0 +1,6 @@
+def solution(arr):
+    answer = sum(arr) / len(arr)
+    return answer
+
+#todo 결과값 시도
+print("평균값 : {}" .format(solution([1,2,3,4])));
---
+++ week_1/program_2.py
@@ -0,0 +1,9 @@
+def solution(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+#* 출력 시도
+print(solution(1))
