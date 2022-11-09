1f487b6749cb4c5946544cbe1039e292c836bfea
marinred <endrms1995@gmail.com>
2022-11-09 19:05:55 +0900

    문제 3번 자릿수 더하기 완료

---
+++ week_1/program_3.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    
+    num = list(str(n)) # num은 str을 list화 한다.
+    for num in str(n):
+        answer += int(num) # num을 int화 하여 각각 더해준다.
+
+    return answer
+
+print(solution(112233))
