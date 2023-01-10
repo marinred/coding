1deaf8eb8e72a1624f05abfc930a44340dc81525
marinred <endrms1995@gmail.com>
2023-01-11 00:02:23 +0900

    프로그래머스 약수의 개수와 덧셈 완료

--- week_1/약수의 개수와 덧셈.py
+++ week_1/약수의 개수와 덧셈.py
@@ -1,28 +1,14 @@
-# def solution(left, right):
-#     answer = 0
-    
-        
-#     return answer
-
-# 어떤 수의 약수 갯수 구하기
-# cnt = 0
-# a = 4
-
-# for i in range(1, a+1):
-#     if a & i == 0:
-#         cnt +=1;
-#     print(cnt)
-    
-# 약수의 갯수가 짝수면 더하고 홀수면 빼게 만들어보자
-
-# cnt = 0
-# a = 4
-# for i in range(1, a+1):
-#     if a % i == 0:
-#         cnt =+1
-# # 홀짝 판단
-# if cnt % 2 == 0:
-#     answer += num
-# else:
-#     answer -= num
-# return answer
+def solution(left, right):
+    answer = 0
+    for i in range(left, right + 1):
+        # 약수의 개수 구하기
+        count = 0
+        for j in range(1, i + 1):
+            if i % j == 0:
+                count += 1
+        # 약수의 개수가 짝수면 더하고 홀수면 빼기
+        if count % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    return answer
