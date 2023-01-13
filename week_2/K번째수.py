d3bad5a4ebacd868546847de70467fcb95956fd6
marinred <endrms1995@gmail.com>
2023-01-13 22:43:02 +0900

    프로그래머스 K번째수 완료

---
+++ week_2/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for i, j, k in commands :
+        new_arr = array[i-1 : j]
+        new_arr.sort()
+        answer.append(new_arr[k-1])
+    return answer
