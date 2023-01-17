98f4a444f9e70091e9d99b2f3ae7e4060c6432d8
marinred <endrms1995@gmail.com>
2023-01-17 19:39:33 +0900

    프로그래머스 비밀지도 완료

--- week_1/비밀지도.py
+++ week_1/비밀지도.py
@@ -1,3 +1,14 @@
 def solution(n, arr1, arr2):
+
     answer = []
+    for i in range(n):
+        row1 = bin(arr1[i])[2:].zfill(n)
+        row2 = bin(arr2[i])[2:].zfill(n)
+        answer.append('')
+        for y in range(n):
+            if row1[y] == '0' and row2[y] == '0':
+                answer[i] += ' '
+            elif row1[y] == '1' or row2[y] == '1':
+                answer[i] += '#'
+
     return answer
